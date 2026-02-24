cce114c51022604e3727237081701c97c8c4705f
rajin572 <din.islam.rajin572@gmail.com>
2026-02-24 11:34:38 +0600

    Update config

--- src/lib/disableConsole.ts
+++
@@ -1,9 +0,0 @@
-export const disableConsole = () => {
-  if (process.env.NODE_ENV === "production") {
-    console.log = () => {};
-    console.warn = () => {};
-    console.error = () => {};
-    console.info = () => {};
-    console.debug = () => {};
-  }
-};
